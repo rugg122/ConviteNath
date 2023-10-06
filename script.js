4be27f18bfd8119ff6c2650af73d05853a96b217
rugg122 <44012135+rugg122@users.noreply.github.com>
2023-10-05 23:59:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const noButtons = document.querySelectorAll(".no");
 const yesButton = document.getElementById('yes');
+const noButtons = document.querySelectorAll('.no');
+
+yesButton.addEventListener('click', () => {
+  Swal.fire({
+    title: 'Ahhh, feliz por poder passar mais um dia com você!!! 😍',
+    html: '<img src="Us.jpg" style="max-width: 100%;" />',
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector('b');
+      const timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 100);
+      Swal.getHtmlContainer().addEventListener('click', () => {
+        clearInterval(timerInterval);
+      });
+    },
+    willClose: () => {
+      // Redirecionamento aqui, após o temporizador expirar
+      window.location.href = 'https://www.youtube.com/watch?v=xnkMVbPia3A';
+    },
+  });
+});
+
+noButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    Swal.fire({
+      title: 'Você clicou em Não Amor? 😭',
+      text: 'Prometo que se Clicar em Sim , vamos nos divertir muitoooo!! hahaha',
+      icon: 'error',
+      confirmButtonText: 'OK',
+    });
+  });
+});
 
 const handleNo = (event) => {
   const noButton = event.currentTarget;
@@ -19,41 +53,18 @@ noButtons.forEach((button) => {
 });
 
 const moveNoButton = (noButton) => {
-  noButton.style.position = "fixed";
+  // Verifique a largura da janela antes de executar a animação
+  if (window.innerWidth > 600) {
+    noButton.style.position = "fixed";
 
-  const rand = Math.random();
+    const rand = Math.random();
 
-  const randTop =
-    (rand * window.innerHeight) % (window.innerHeight - noButton.offsetHeight);
-  const randLeft =
-    (window.innerWidth * rand * 10) % (window.innerWidth - noButton.offsetWidth);
+    const randTop =
+      (rand * window.innerHeight) % (window.innerHeight - noButton.offsetHeight);
+    const randLeft =
+      (window.innerWidth * rand * 10) % (window.innerWidth - noButton.offsetWidth);
 
-  noButton.style.top = `${randTop}px`;
-  noButton.style.left = `${randLeft}px`;
+    noButton.style.top = `${randTop}px`;
+    noButton.style.left = `${randLeft}px`;
+  }
 };
-
-yesButton.addEventListener("click", () => {
-  let timerInterval;
-  Swal.fire({
-    title: 'Ahhh Feliz por poder passar mais um dia com você!!! 😍',
-    html: '<img src="Us.jpg" style="max-width: 100%;" />',
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: () => {
-      Swal.showLoading();
-      const b = Swal.getHtmlContainer().querySelector('b');
-      timerInterval = setInterval(() => {
-        b.textContent = Swal.getTimerLeft();
-      }, 100);
-    },
-    willClose: () => {
-      clearInterval(timerInterval);
-      // Redirecionamento aqui, após o temporizador expirar
-      window.location.href = 'https://www.youtube.com/watch?v=xnkMVbPia3A';
-    }
-  }).then((result) => {
-    if (result.dismiss === Swal.DismissReason.timer) {
-      console.log('Fechado pelo temporizador');
-    }
-  });
-});
